82bfd7bfb426fff7b5cc12290f4628d25fb497a0
diegofly91 <diego_fernandolibreros@hotmail.com>
2022-02-23 00:01:12 -0500

    add header component

--- src/__test__/components/Footer.test.tsx
+++ src/__test__/components/Footer.test.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import Footer from '../../components/Footer';
 import { render, screen } from '@testing-library/react'
-import Home from '@/pages/index'
 
 
 describe('Footer', () => {
-  it('renders a heading', () => {
+  it('renders a Footer', () => {
     render(<Footer />)
 
-    const heading = screen.getByText('Diego Libreros')
+    const footer = screen.getByText('Diego Libreros')
 
-    expect(heading).toBeInTheDocument()
+    expect(footer).toBeInTheDocument()
   })
 })
