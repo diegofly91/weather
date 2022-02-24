c363deefe098cf5e911e60b3d46d130f0d5ee3bc
diegofly91 <diego_fernandolibreros@hotmail.com>
2022-02-24 15:40:56 -0500

    Updates

---
+++ _next/static/iYPspa5wOhBP_SrcZg3FB/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/235-fe642252249e5ea7.js","static/css/658b22076b6d55f6.css","static/chunks/pages/index-0deb3e7809e07bd7.js"],"/_error":["static/chunks/pages/_error-2f883067a14f4c4a.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
